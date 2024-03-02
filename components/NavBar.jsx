ec44f517d2cb53ed6f17c0bf62231306979711b2
Patric Garganian <jpgarganian@gmail.com>
2024-03-02 08:50:36 +0800

    Update

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -23,13 +23,7 @@ export default function NavBar() {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image
-    <nav class="bg-white border-gray-250 border dark:bg-gray-900">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
+
             src="https://flowbite.com/docs/images/logo.svg"
             width={32}
             height={32}
